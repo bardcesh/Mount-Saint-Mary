447c929dd77799ab5890c3a459dc680e7d0abb6d
bardcesh <156127057+bardcesh@users.noreply.github.com>
2024-09-06 13:20:16 -0400

    Update gaction_test.r
    copied from Kingston

--- gaction_test.r
+++ gaction_test.r
@@ -1,3 +1,4 @@
+#Load library
 library(tidyverse)
 library(lubridate)
 library(httr)
@@ -5,123 +6,174 @@ library(jsonlite)
 
 #### ---------- API stuff
 
-## QuantAQ API details
+## QuantAQ API:
+
 api_key = 'JSPWKD9RVB5IMP526YLBSGCS'
 serial_number = 'MOD-PM-00215' # Specific to this monitoring device
 
 base_url = "https://api.quant-aq.com/device-api/v1"
+accounts_endpoint = '/account'
+# data_endpoint = paste0('/',serial_number,'/data')
 data_endpoint = '/data'
+raw_data_endpoint = '/data/raw'
 
-# Function to make API requests
+
 get_request = function(url, api_key, params = NULL){
   response = httr::GET(
     url = url,
-    authenticate(user = api_key, password = api_key, type = 'basic'),
+    authenticate(
+      user = api_key, password = api_key, type = 'basic'),
     query = params
   )
   return(response)
 }
 
-# Function to get data from the API
+
+# data_points=5000
 get_data = function(serial_number, data_points=NULL, start_date=NULL, end_date = NULL){
-  # This will save all the data
-  main_data = list()  # Use list to accumulate data
+  # this will save all the data
+  main_data = c()
   
-  # Data endpoint for a specific defined serial number
-  url = paste0(base_url, '/devices/', serial_number, data_endpoint)
+  # adding serial number in endpoint
+  data_endpoint_with_serial = paste0('/devices','/', serial_number, data_endpoint)
   
-  # Adding date filter if it's not null in parameters
+  # data endpoint for a specific defined serial number
+  url = paste0(base_url, data_endpoint_with_serial)
+  
+  # adding date filter if its not null in parameters
   date_filter = NULL
-  if (!is.null(start_date) || !is.null(end_date)) {
-    date_filter = paste0("timestamp_local,ge,", start_date, ";timestamp_local,le,", end_date)
+  if (all(!is.null(start_date) || !is.null(end_date))) {
+    date_filter = paste0("timestamp_local,ge,",start_date,";timestamp_local,le,",end_date)
   }
+  # different parameters we are sending with request
+  params = list(
+    page=1,limit=data_points,sort="timestamp_local,desc",
+    per_page=1000, filter=date_filter)
   
-  # Parameters for the request
-  params = list(page=1, limit=data_points, sort="timestamp_local,desc", per_page=1000, filter=date_filter)
-  
-  # Loop to handle pagination
-  repeat {
+  # for multiple page scrape we add a loop
+  index = 1
+  repeat{
     response = get_request(url = url, api_key = api_key, params = params)
     response_data = content(response, 'parsed', encoding = 'UTF-8')
-    
-    if (length(response_data$data) == 0) {
-      print('No more data available.')
-      break
-    }
-    
-    main_data <- c(main_data, response_data$data)  # Accumulate data
+    main_data = c(main_data, response_data$data)
     url = response_data$meta$next_url
-    
-    if (is.null(url)) {
-      print('No more pages.')
-      break
+    if(!is.null(url)){
+      index = index + 1
+      params$page = index
+    }else{
+      print('break')
+      break()
     }
-    
-    params$page = params$page + 1
   }
-  
-  print('Total number of data points:')
+  print('total no of data points')
   print(length(main_data))
-  
-  # Convert the list of data to JSON
-  json_data = toJSON(main_data, auto_unbox = TRUE, pretty = TRUE)
+  json_data = toJSON(main_data,auto_unbox = TRUE, pretty = TRUE)
   return(json_data)
 }
 
-##### ----- Data pull and munging
+get_raw_data = function(serial_number, data_points=NULL, start_date=NULL, end_date = NULL){
+  # this will save all the data
+  main_raw_data = c()
+  
+  # adding serial number in endpoint
+  raw_data_endpoint_with_serial = paste0('/devices','/', serial_number, raw_data_endpoint)
+  
+  # data endpoint for a specific defined serial number
+  url = paste0(base_url, raw_data_endpoint_with_serial)
+  
+  # adding date filter if its not null in parameters
+  date_filter = NULL
+  if (all(!is.null(start_date) || !is.null(end_date))) {
+    date_filter = paste0("timestamp_local,ge,",start_date,";timestamp_local,le,",end_date)
+  }
+  # different parameters we are sending with request
+  params = list(
+    page=1,limit=data_points,sort="timestamp_local,desc",
+    per_page=1000, filter=date_filter)
+  
+  # for multiple page scrape we add a loop
+  index = 1
+  repeat{
+    response = get_request(url = url, api_key = api_key, params = params)
+    response_data = content(response, 'parsed', encoding = 'UTF-8')
+    main_raw_data = c(main_raw_data, response_data$data)
+    url = response_data$meta$next_url
+    if(!is.null(url)){
+      index = index + 1
+      params$page = index
+    }else{
+      print('break')
+      break()
+    }
+  }
+  print('total no of data points')
+  print(length(main_raw_data))
+  json_data = toJSON(main_raw_data, auto_unbox = TRUE, pretty = TRUE)
+  return(json_data)
+}
 
-# Read the existing CSV file
-wk <- read_csv("https://raw.githubusercontent.com/bardcesh/Mount-Saint-Mary/main/data/full_newburgh.csv", col_types = "cddc") 
 
-# Convert YMD to datetime
+##### -----  Below is the action data pull and munging
+
+
+wk <- read_csv("https://raw.githubusercontent.com/bardcesh/Kingston-Quant-AQ/main/data/full_newburgh.csv", col_types = "cddc") 
+
+
 wk <- wk %>%
-  mutate(YMD = ymd_hms(YMD))
+  mutate(
+    YMD = ymd_hms(YMD)
+  )
 
-# Find the latest date in the .csv file
+# Find latest date in .csv file
 max_date <- max(wk$YMD)
 
-# Fetch new data from API
-data = get_data(serial_number = serial_number, data_points=NULL, start_date=max_date, end_date=floor_date(now("EST"), unit="hours"))
-
-# Convert JSON data to a list or data frame
-recent_data <- jsonlite::fromJSON(data)
-
-# Check if recent_data is a list and convert to data frame if necessary
-if (is.list(recent_data)) {
-  print("Structure of recent_data:")
-  print(str(recent_data))
-  
-  # Extract the relevant data component
-  recent_data <- recent_data$data.frame  # Adjust based on actual JSON structure
-}
-
-# Ensure recent_data is a data frame
-if (!is.data.frame(recent_data)) {
-  stop("Recent data is not a data frame.")
-}
-
-# Process the data
-recent_data <- recent_data %>%
-  select(timestamp_local, pm25, pm10) %>%
-  rename(
+# Do a call to API to get rest of data
+# making time floor to start of hour so that it does not add partial hours to the spreadhseet
+data = get_data(serial_number =  serial_number, data_points=NULL, start_date=max_date, end_date = floor_date(now("EST"), unit = "hours"))
+recent_data <- jsonlite::fromJSON(data) %>%
+  # Need to make this section resilient to there being no new datapoints 
+  select( #selects certain variables from dataset 
+    timestamp_local, pm25, pm10
+  ) %>%
+  rename( # Renames them so that they display nicely in plots
     PM2.5 = pm25,
     PM10 = pm10
-  ) %>%
-  mutate(timestamp_local = ymd_hms(timestamp_local)) %>%
-  rename(YMD = timestamp_local) %>%
+  ) 
+
+
+recent_data$timestamp_local <- ymd_hms(recent_data$timestamp_local)
+
+recent_data <- recent_data %>%
+  rename(
+    YMD = timestamp_local
+  )  %>%
   mutate(
     PM2.5 = as.numeric(PM2.5),
     PM10 = as.numeric(PM10)
   )
 
-# Compress to hourly data
+# Compress to hourly
+
 recent_data_h <- recent_data %>%
-  group_by(YMD = cut(YMD, breaks="60 min")) %>%
+  mutate(YMD = floor_date(YMD, unit = "hour")) %>%  # Floor the timestamps to the nearest hour
+  group_by(YMD) %>%
   summarize(
     PM2.5 = mean(PM2.5, na.rm=TRUE),
     PM10 = mean(PM10, na.rm=TRUE)
-  ) %>%
-  mutate(YMD = ymd_hms(YMD))
+  )
 
-# Append new data to the existing CSV file
-write_csv(recent_data_h, 'data/full_newburgh.csv', append = TRUE)
+
+
+
+
+
+
+
+
+# create a new row of data, with todayd's date and urls number
+#row <- data.frame(Sys.time(), recent_data$PM2.5)
+
+#print(row)
+
+# append at the end of the csv the new data
+write_csv(recent_data_h,paste0('data/full_newburgh.csv'),append = T)   
